19e1f85c1ad60bc51d368447e4ac42b834381fad
Mike Durst <mdurst@inspireenergy.com>
2016-11-10 15:53:20 -0500

    Remove puts

--- lib/rubillow/models/deep_search_results.rb
+++ lib/rubillow/models/deep_search_results.rb
@@ -2,16 +2,7 @@ module Rubillow
   module Models
     # List of comps for a given property with deep data.
     class DeepSearchResults < Base
-      # @return [Models::DeepSearchResult] principal property.
-      attr_accessor :principal
-      
-      # @return [Hash] comparables list (key => comparable's score, value => {Models::DeepSearchResult}).
-      #
-      # @example
-      #  comparables.each do |score, comp|
-      #    puts score
-      #    puts comp.price
-      #  end
+      # @return [Hash] results list
       attr_accessor :results
       
       protected
@@ -22,11 +13,9 @@ module Rubillow
         
         return if !success?
         
-        # @principal = DeepSearchResult.new(@parser.xpath('//principal').to_xml)
-        
         @results = Array.new
         @parser.xpath('//result').each do |elm|
-          puts elm.to_s
+          # puts elm.to_s
           # key = elm.attribute('zpid').value
           @results.push DeepSearchResult.new(elm.to_xml)
         end
